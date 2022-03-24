337eca5684827e86a36c38465dfc50f9cc157067
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-24 16:41:24 -0500

    JAVA Intro lecture and exercises

---
+++ src/YoHey.java
@@ -0,0 +1,88 @@
+public class YoHey {
+    public static void main(String[] args) {
+////        System.out.println("Yo, Hey");
+//
+//        //Data Types
+////        byte 	1 	Very short integers from -128 to 127
+//        byte myBite = 127;
+//        System.out.println("myBite = " + myBite);
+//
+////        short 	2 	Short integers from -32,768 to 32,767
+//        short myShort = -31000;
+//        System.out.println("myShort = " + myShort);
+//
+//        myShort = 20;
+//
+//        System.out.println("myShort = " + myShort);
+//
+////        int 	4 	Integers from -2,147,483,648 to 2,147,483,647
+//        int myInt = myShort; //implicit
+//        myInt = (int) myBite; //explicit
+//
+//        System.out.println("myInt = " + myInt);
+//
+////        long 	8 	Long integers from -9,223,372,036,854,775,808 to 9,223,372,036,           854,775,807
+//        long myLong = 100_000_000L;
+//
+//        System.out.println("myLong = " + myLong);
+//
+////        float 	4 	Single-precision, floating-point numbers from -3.4E38 to              3.4E38 with up to 7 significant digits
+//        float myFloat = 1.234567890123F;
+//        System.out.println("myFloat = " + myFloat);
+//
+////        double 	8 	Double-precision, floating-point numbers from -1.7E308 to             1.7E308 with up to 16 significant digits
+//        double myDouble = 10.12123453520546545D;
+//        System.out.println("myDouble = " + myDouble);
+//
+////        char 	2 	A single Unicode character that’s stored in two bytes
+//        char myChar = 'C';
+////
+////        boolean 	1 	A true or false value
+//        boolean isTrue = true;
+//        System.out.println("isTrue = " + isTrue);
+//
+//        //Reference data types:
+//            //default to null if not assigned anything
+//
+//        //String
+//        String myString = "Double Quotes!!!! For Strings";
+//        System.out.println("myString = " + myString);
+//
+//        //Escape Characters
+//        myString = "  \" Hello \"   \\  ";
+//        System.out.println("myString = " + myString);
+
+//Syntax, Types, and Variables Exercise:
+        int myFavoriteNUmber = 8;
+        System.out.println("myFavoriteNumber is " + myFavoriteNUmber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        System.out.println(three);
+//
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
