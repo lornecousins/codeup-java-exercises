4bc6ec1e9951eb7f06f5ec79a851e4835d74a663
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-28 16:34:01 -0500

    refactored 1a

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -13,7 +13,7 @@ public class ControlFlowExercises {
 
         int i = 5;
         while (i <= 15) {
-            System.out.println(i++);
+            System.out.print(i++ + " ");
         }
     }
 }
