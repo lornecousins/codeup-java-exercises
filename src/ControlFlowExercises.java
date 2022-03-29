1a8014fada1e8f53ac1e7877aa9ad92c313120dd
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 08:56:27 -0500

    exercise 1c-c complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,13 +2,12 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         //1c For
         //
-        //Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        //Alter your loop to count backwards by 5's from 100 to -10.
 
         int i;
-        for (i = 0; i <= 100; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
+        for (i = 100; i >= -10; i--) {
+            System.out.println(i + " ");
+            i = i -4;
         }
     }
 }
