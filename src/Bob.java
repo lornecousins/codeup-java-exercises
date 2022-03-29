ff123828af19c75e048c6b358be5f576f1c5070d
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 15:59:15 -0500

    Convo with Bob complete

---
+++ src/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What's up?");
+        do {
+        String conversation = sc.nextLine();
+        if (conversation.endsWith("? ")) {
+            System.out.println("Sure");
+        } else if (conversation.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (conversation.equals("")) {
+                System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+        } while (true);
+    }
+}
