34b7458fe0e015b681ea9b2bf481ada3b54eebe2
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 09:39:12 -0500

    exercise 1c-d complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //1c For
-        //
-        //Alter your loop to count backwards by 5's from 100 to -10.
+        //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//        2
+//        4
+//        16
+//        256
+//        65536
 
-        int i;
-        for (i = 100; i >= -10; i--) {
+        long i;
+        for (i = 2; i < 1000000; i *= i)
             System.out.println(i + " ");
-            i = i -4;
-        }
     }
 }
