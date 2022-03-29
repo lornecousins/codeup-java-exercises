505124662849c499f1616cd3783af2bb7f425b39
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 09:48:07 -0500

    exercise 2a complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        //1c For
-        //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-//        2
-//        4
-//        16
-//        256
-//        65536
+        //2a Write a program that prints the numbers from 1 to 100.
 
-        long i;
-        for (i = 2; i < 1000000; i *= i)
-            System.out.println(i + " ");
+        int i;
+        for (i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
