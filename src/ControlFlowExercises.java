6acc2ba197c808adab89acf7e325e275f417f9cb
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 10:00:03 -0500

    exercise 2b,c,d complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,21 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        //2a Write a program that prints the numbers from 1 to 100.
+        //2 Write a program that prints the numbers from 1 to 100.
+        //For multiples of three: print “Fizz” instead of the number
+        //For the multiples of five: print “Buzz”.
+        //For numbers which are multiples of both three and five: print “FizzBuzz”.
 
-        int i;
-        for (i = 1; i <= 100; i++) {
-            System.out.print(i + " ");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (((i % 3) == 0) && ((i % 5) == 0)) // Is it a multiple of 3 & 5?
+                System.out.print("fizzbuzz" + " ");
+            else if ((i % 3) == 0) // Is it a multiple of 3?
+                System.out.print("fizz" + " ");
+            else if ((i % 5) == 0) // Is it a multiple of 5?
+                System.out.print("buzz" + " ");
+            else
+                System.out.print(i + " "); // Not a multiple of 3 or 5
         }
     }
-}
+    }
+
