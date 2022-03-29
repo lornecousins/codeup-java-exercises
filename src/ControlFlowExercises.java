62f332665fb503cc34c282a2b605466a459b4554
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 10:33:44 -0500

    exercise 3 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,21 +1,46 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        //2 Write a program that prints the numbers from 1 to 100.
-        //For multiples of three: print “Fizz” instead of the number
-        //For the multiples of five: print “Buzz”.
-        //For numbers which are multiples of both three and five: print “FizzBuzz”.
+        //3
+        // Display a table of powers.
+        //
+        //    Prompt the user to enter an integer.
+        //    Display a table of squares and cubes from 1 to the value entered.
+        //    Ask if the user wants to continue.
+        //    Assume that the user will enter valid data.
+        //    Only continue if the user agrees to.
+        //
+        //Example Output
+        //
+        //What number would you like to go up to? 5
+        //
+        //Here is your table!
+        //
+        //number | squared | cubed
+        //------ | ------- | -----
+        //1      | 1       | 1
+        //2      | 4       | 8
+        //3      | 9       | 27
+        //4      | 16      | 64
+        //5      | 25      | 125
 
-        for (int i = 1; i <= 100; i++)
-        {
-            if (((i % 3) == 0) && ((i % 5) == 0)) // Is it a multiple of 3 & 5?
-                System.out.print("fizzbuzz" + " ");
-            else if ((i % 3) == 0) // Is it a multiple of 3?
-                System.out.print("fizz" + " ");
-            else if ((i % 5) == 0) // Is it a multiple of 5?
-                System.out.print("buzz" + " ");
-            else
-                System.out.print(i + " "); // Not a multiple of 3 or 5
+        System.out.println("Welcome to the Squares and Cubes Table");
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an Integer: ");
+        int number = sc.nextInt();
+        System.out.println(("Number" + "  " + "Squared" + "  " + "Cubed"));
+        System.out.println("------ |" + " ------ |" + " ------");
+        for (int i = 1; i <= number; i++) {
+//            i = number;
+            int numberSquared = (int) Math.pow(i, 2);
+            int numberCubed = (int) Math.pow(i, 3);
+        String message = "\n" + i + "        " + numberSquared + "        " + numberCubed;
+            System.out.println(message);
+            System.out.println();
         }
+
     }
     }
 
--- src/consoleIoLecture.java
+++ src/consoleIoLecture.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class consoleIoLecture {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-////        String userInput = sc.next();
-//        System.out.println("You entered: " + userInput);
-//        System.out.print("Please enter your age: ");
-//        int age = sc.nextInt();
-//        System.out.println(age);
+        String userInput = sc.next();
+        System.out.println("You entered: " + userInput);
+        System.out.print("Please enter your age: ");
+        int age = sc.nextInt();
+        System.out.println(age);
 
-//        System.out.print("Favorite quote: ");
-//        String quote = sc.nextLine();
-//        System.out.println(quote);
+        System.out.print("Favorite quote: ");
+        String quote = sc.nextLine();
+        System.out.println(quote);
 
         //Quirk of using next() then nextLine()..
 
