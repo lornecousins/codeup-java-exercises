c32d6bfbb34d10ea9c55d2330941ef5b3388e7f4
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-29 15:24:19 -0500

    StringLecture and StringExercise #1

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        //1
+        String pink = "We don't need no education";
+        System.out.println(pink);
+
+        String floyd = "We don't need no thought control";
+        System.out.println(floyd);
+
+        String yo = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(yo);
+
+        String windows = "In windows the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String slashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\!";
+        System.out.println(slashes);
+    }
+}
