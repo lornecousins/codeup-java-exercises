0983eedd5b186132a015d6a21011f3f7162270ab
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-30 16:05:38 -0500

    Methods Exercise 2 complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -28,46 +28,56 @@ public class MethodsExercises {
 //    Methods Exercises:
         //1
 
-        int num1, num2, sum, difference, product, division, modulus;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("First Number: ");
-        num1 = sc.nextInt();
-        System.out.print("Second Number: ");
-        num2 = sc.nextInt();
-        sum = addTwo(num1, num2);
-        difference = subtractTwo(num1, num2);
-        product = multiplyTwo(num1, num2);
-        division = divideTwo(num1, num2);
-        modulus = modulateTwo(num1, num2);
-        System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + difference);
-        System.out.println("Product: " + product);
-        System.out.println("Division: " + division);
-        System.out.println("Modulus: " + modulus);
-    }
-        public static int addTwo(int a, int b) {
-        int sum = a + b;
-        return sum;
-    }
-        public static int subtractTwo(int a, int b) {
-            int difference = a - b;
-            return difference;
-    }
-        public static int multiplyTwo(int a, int b) {
-            int product = a * b;
-            return product;
-    }
-        public static int divideTwo(int a, int b) {
-            int division = a / b;
-            return division;
-    }
-        public static int modulateTwo(int a, int b) {
-        int modulus = a % b;
-        return modulus;
+//        int num1, num2, sum, difference, product, division, modulus;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("First Number: ");
+//        num1 = sc.nextInt();
+//        System.out.print("Second Number: ");
+//        num2 = sc.nextInt();
+//        sum = addTwo(num1, num2);
+//        difference = subtractTwo(num1, num2);
+//        product = multiplyTwo(num1, num2);
+//        division = divideTwo(num1, num2);
+//        modulus = modulateTwo(num1, num2);
+//        System.out.println("Sum: " + sum);
+//        System.out.println("Difference: " + difference);
+//        System.out.println("Product: " + product);
+//        System.out.println("Division: " + division);
+//        System.out.println("Modulus: " + modulus);
+//    }
+//        public static int addTwo(int a, int b) {
+//        int sum = a + b;
+//        return sum;
+//    }
+//        public static int subtractTwo(int a, int b) {
+//            int difference = a - b;
+//            return difference;
+//    }
+//        public static int multiplyTwo(int a, int b) {
+//            int product = a * b;
+//            return product;
+//    }
+//        public static int divideTwo(int a, int b) {
+//            int division = a / b;
+//            return division;
+//    }
+//        public static int modulateTwo(int a, int b) {
+//        int modulus = a % b;
+//        return modulus;
 
 
         //2
+        int validNumber = getInteger(1, 10);
+        }
 
+        public static int getInteger(int min, int max) {
+            int userInput;
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.print("Enter a number between " + min + " and " + max + ": ");
+                userInput = sc.nextInt();
+            } while (userInput < min || userInput > max);
+            return userInput;
     }
 }
 
