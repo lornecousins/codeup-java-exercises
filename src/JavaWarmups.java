265d52bfba2b16cafea7bec341545ca850197f5f
Lorne Cousins <lorne.cousins88@gmail.com>
2022-03-31 10:14:56 -0500

    create class for JavaWarmups and completed first warmup

---
+++ src/JavaWarmups.java
@@ -0,0 +1,20 @@
+public class JavaWarmups {
+    public static char returnFirstCapitalLetter(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == Character.toUpperCase(str.charAt(i))) {
+                return str.charAt(i);
+            }
+        }
+              return ' ';
+}
+
+public static void main(String[] args) {
+    System.out.println(returnFirstCapitalLetter("hellO"));
+    System.out.println(returnFirstCapitalLetter("hello"));
+    System.out.println(returnFirstCapitalLetter("hEllo"));
+    System.out.println(returnFirstCapitalLetter("heLlo"));
+    System.out.println(returnFirstCapitalLetter("H"));
+}
+    }
+
