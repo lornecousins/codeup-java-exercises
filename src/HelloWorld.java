0dde755ac1977a1a56f924e7a2d42d594eee93d1
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-05 14:32:08 -0500

    Java Syntax, Data Types and Variables Exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,43 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFavoriteNUmber = 8;
+        System.out.println("myFavoriteNumber is " + myFavoriteNUmber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int class
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        System.out.println("x = " + x);
+
+//        int x = 3;
+//        int y = 4;
+//       y *= x;
+//        System.out.println("y = " + y);
+
+        int x = 10;
+        int y = 2;
+        x /=   y;
+        y -=   x;
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+
     }
 }
